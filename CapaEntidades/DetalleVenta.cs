f93b0505edfb68aea87694d7419485bec87f52e2
Alexis Rauda <Alexis Rauda@DESKTOP-03PS3SB>
2024-04-04 18:38:43 -0600

    Primer Push

--- CapaEntidades/DetalleVenta.cs
+++ CapaEntidades/DetalleVenta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace CapaEntidades
 {
     public class DetalleVenta
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DetalleVentaId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public  decimal Precio { get; set; }
+        [Required]
+        public int ProductoId { get; set; }
+        public Producto Producto { get; set; }
+        [Required]
+        public int VentaId { get; set; }
+        public Venta Venta { get; set; }
+
     }
 }
