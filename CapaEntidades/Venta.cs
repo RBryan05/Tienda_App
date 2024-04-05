c876b1df201e85717ca80588a8bfb48289081d39
Alexis Rauda <Alexis Rauda@DESKTOP-03PS3SB>
2024-04-04 18:35:24 -0600

    Creacion de repositorio

--- CapaEntidades/Venta.cs
+++ CapaEntidades/Venta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,15 @@ namespace CapaEntidades
 {
     public class Venta
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public  int VentaId { get; set; }
+        [Required]
+        public  DateTime Fecha { get; set; }
+        [Required]
+        public decimal Total { get; set; }
 
+        // propiedades de navegacion
+        public List<DetalleVenta> Detalles { get; set; }
     }
 }
