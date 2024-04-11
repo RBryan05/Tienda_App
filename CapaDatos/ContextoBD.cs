0e9051db587307f923f5b219725578b87c6dc186
Alexis Rauda <Alexis Rauda@DESKTOP-03PS3SB>
2024-04-11 17:46:44 -0600

    ContextoBD

--- CapaDatos/ContextoBD.cs
+++ CapaDatos/ContextoBD.cs
@@ -1,5 +1,7 @@
+using CapaEntidades;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
 
@@ -8,13 +10,19 @@ namespace CapaDatos
     public partial class ContextoBD : DbContext
     {
         public ContextoBD()
-            : base("name=ContextoBD")
+            : base(ConfigurationManager.ConnectionStrings["ContextoBD"].ConnectionString)
         {
         }
 
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-        }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //}
+
+        public virtual DbSet<Venta> Ventas { get; set; }
+
+        public virtual DbSet<Producto> Productos { get; set; }
+
+        public  virtual DbSet<DetalleVenta> DetalleVenta { get; set; }
     }
 }
